2ce15feaba8faeece80911b05daf3d169baceef7
Webmap2021 <76879681+Webmap2021@users.noreply.github.com>
2021-01-02 12:41:04 -0800

    Add files via upload

---
+++ data/Musanze_hotels_1.js
@@ -0,0 +1,21 @@
+var json_Musanze_hotels_1 = {
+"type": "FeatureCollection",
+"name": "Musanze_hotels_1",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "gid": 1.0, "names": "Five Volcanoes Boutique Hotel", "x": 29.5935523, "y": -1.4483244 }, "geometry": { "type": "Point", "coordinates": [ 29.5935523, -1.4483244 ] } },
+{ "type": "Feature", "properties": { "gid": 2.0, "names": "La Paillotte Gorilla Place", "x": 29.5769061, "y": -1.4463411 }, "geometry": { "type": "Point", "coordinates": [ 29.5769061, -1.4463411 ] } },
+{ "type": "Feature", "properties": { "gid": 3.0, "names": "One&Only Gorilla's Nest", "x": 29.5774211, "y": -1.4496016 }, "geometry": { "type": "Point", "coordinates": [ 29.5774211, -1.4496016 ] } },
+{ "type": "Feature", "properties": { "gid": 4.0, "names": "Amarembo Gorilla Village", "x": 29.5769061, "y": -1.4463411 }, "geometry": { "type": "Point", "coordinates": [ 29.5769061, -1.4463411 ] } },
+{ "type": "Feature", "properties": { "gid": 5.0, "names": "Fatima Hotel", "x": 29.6326228, "y": -1.5006145 }, "geometry": { "type": "Point", "coordinates": [ 29.6326228, -1.5006145 ] } },
+{ "type": "Feature", "properties": { "gid": 6.0, "names": "Fatima Hotel", "x": 29.6181906, "y": -1.4803189 }, "geometry": { "type": "Point", "coordinates": [ 29.6181906, -1.4803189 ] } },
+{ "type": "Feature", "properties": { "gid": 7.0, "names": "The Garden Place Hotel", "x": 29.6284045, "y": -1.4922454 }, "geometry": { "type": "Point", "coordinates": [ 29.6284045, -1.4922454 ] } },
+{ "type": "Feature", "properties": { "gid": 8.0, "names": "Isange Paradise Resort", "x": 29.610895, "y": -1.4922454 }, "geometry": { "type": "Point", "coordinates": [ 29.610895, -1.4922454 ] } },
+{ "type": "Feature", "properties": { "gid": 9.0, "names": "Migano hotel", "x": 29.6369639, "y": -1.5087616 }, "geometry": { "type": "Point", "coordinates": [ 29.6369639, -1.5087616 ] } },
+{ "type": "Feature", "properties": { "gid": 10.0, "names": "Best View hotel", "x": 29.6295181, "y": -1.5003853 }, "geometry": { "type": "Point", "coordinates": [ 29.6295181, -1.5003853 ] } },
+{ "type": "Feature", "properties": { "gid": 11.0, "names": "La Palme Hotel", "x": 29.6300385, "y": -1.5000099 }, "geometry": { "type": "Point", "coordinates": [ 29.6300385, -1.5000099 ] } },
+{ "type": "Feature", "properties": { "gid": 12.0, "names": "Hotel Muhabura", "x": 29.6300385, "y": -1.5000099 }, "geometry": { "type": "Point", "coordinates": [ 29.6300385, -1.5000099 ] } },
+{ "type": "Feature", "properties": { "gid": 13.0, "names": "Gorillas Volcanoes Hotel", "x": 29.6322112, "y": -1.5005487 }, "geometry": { "type": "Point", "coordinates": [ 29.6322112, -1.5005487 ] } },
+{ "type": "Feature", "properties": { "gid": 14.0, "names": "Faraja Hotel", "x": 29.6212474, "y": -1.5013126 }, "geometry": { "type": "Point", "coordinates": [ 29.6212474, -1.5013126 ] } }
+]
+}
---
+++ data/Musanze_tourist_attraction_5.js
@@ -0,0 +1,15 @@
+var json_Musanze_tourist_attraction_5 = {
+"type": "FeatureCollection",
+"name": "Musanze_tourist_attraction_5",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "gid": 1.0, "names": "Musanze cave", "x": 29.5154617, "y": -1.4845275, "Photo": "Ĭ" }, "geometry": { "type": "Point", "coordinates": [ 29.5154617, -1.4845275 ] } },
+{ "type": "Feature", "properties": { "gid": 2.0, "names": "red rock", "x": 29.6012135, "y": -1.5603075, "Photo": "Ĭ" }, "geometry": { "type": "Point", "coordinates": [ 29.6012135, -1.5603075 ] } },
+{ "type": "Feature", "properties": { "gid": 3.0, "names": "Virunga national park", "x": 29.5315932, "y": -1.4320737, "Photo": "Ĭ" }, "geometry": { "type": "Point", "coordinates": [ 29.5315932, -1.4320737 ] } },
+{ "type": "Feature", "properties": { "gid": 4.0, "names": "BUHANGA ECO-PARK", "x": 29.5997682, "y": -1.5706118, "Photo": "Ĭ" }, "geometry": { "type": "Point", "coordinates": [ 29.5997682, -1.5706118 ] } },
+{ "type": "Feature", "properties": { "gid": 5.0, "names": "Musanze Tourism Information Center", "x": 29.5980308, "y": -1.5021168, "Photo": "Ĭ" }, "geometry": { "type": "Point", "coordinates": [ 29.5980308, -1.5021168 ] } },
+{ "type": "Feature", "properties": { "gid": 6.0, "names": "Ruhondo Beach Resort", "x": 29.708409, "y": -1.4964539, "Photo": "Ĭ" }, "geometry": { "type": "Point", "coordinates": [ 29.708409, -1.4964539 ] } },
+{ "type": "Feature", "properties": { "gid": 7.0, "names": "Volcanoes National Park Headquarters", "x": 29.5315932, "y": -1.4320737, "Photo": "Ĭ" }, "geometry": { "type": "Point", "coordinates": [ 29.5315932, -1.4320737 ] } },
+{ "type": "Feature", "properties": { "gid": 8.0, "names": "Waterfall", "x": 29.69372, "y": -1.56061, "Photo": "Ĭ" }, "geometry": { "type": "Point", "coordinates": [ 29.69372, -1.56061 ] } }
+]
+}
